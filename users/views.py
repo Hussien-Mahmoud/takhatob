a111922ae08a4231e0423067680d4af1c6734d3b
Hussien-Mahmoud <hussienmahmuod@gmail.com>
2023-04-26 00:32:55 +0200

    fixing sign_up view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseNotAllowed
 from django.urls import reverse_lazy
-# from django.contrib.auth import login
+from django.contrib.auth import login
 
 
 from .forms import ClientSignUpForm
@@ -19,30 +19,9 @@ def sign_up(request):
             user = form.save()
             login(request, user)
             return redirect(reverse_lazy('home'))
-        print(form.errors)
     else:
         return HttpResponseNotAllowed(['GET', 'POST'])
 
     return render(request, 'registration/sign_up.html', {
         'form': form
     })
-
-#
-# def login(request):
-#
-#     if request.method == 'GET':
-#         form = ClientSignUpForm()
-#
-#     elif request.method == 'POST':
-#         form = ClientSignUpForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect(reverse_lazy('home'))
-#         print(form.errors)
-#     else:
-#         return HttpResponseNotAllowed(['GET', 'POST'])
-#
-#     return render(request, 'registration/sign_up.html', {
-#         'form': form
-#     })
