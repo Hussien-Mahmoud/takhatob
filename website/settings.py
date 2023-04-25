05456c95afe85fdaa830a796a1d8120af7e96fde
Hussien-Mahmoud <hussienmahmuod@gmail.com>
2023-04-25 22:29:00 +0200

    add CSRF_TRUSTED_ORIGINS and ALLOWED_HOSTS

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,8 @@ CSRF_TRUSTED_ORIGINS = ['https://takhatob-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
