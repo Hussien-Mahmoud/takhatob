b1ced5f1293ca246fa7540ce4bfd16511985baed
Hussien-Mahmoud <hussienmahmuod@gmail.com>
2023-06-25 03:38:10 +0300

    updating asgi.py

--- website/asgi.py
+++ website/asgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 import django
+
 django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
@@ -23,7 +25,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        ),
     ),
 })
